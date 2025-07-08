90454cd8a5587dbd33c8af2af9f46e48ec897844
Cesar Luis RR <romcel87@gmail.com>
2025-07-08 16:32:58 -0500

    mp

--- resources/views/woocommerce/checkout/payment.blade.php
+++ resources/views/woocommerce/checkout/payment.blade.php
@@ -1,28 +1,28 @@
 <div class="woocommerce-checkout-payment">
-    @php
-        if (WC()->cart->needs_payment()) {
+    @if (WC()->cart->needs_payment())
+        @php
             do_action('woocommerce_review_order_before_payment');
             $available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
-        }
-    @endphp
+        @endphp
 
-    <ul class="space-y-4">
-        @foreach ($available_gateways as $gateway)
-            <li class="border rounded-lg p-4 @if($gateway->chosen) border-blue-600 @else border-gray-300 @endif">
-                <label class="flex items-center space-x-2">
-                    <input type="radio" name="payment_method" value="{{ $gateway->id }}" class="accent-blue-600" @if($gateway->chosen) checked @endif>
-                    <span class="font-medium text-sm text-gray-800">{{ $gateway->get_title() }}</span>
-                </label>
+        <ul class="space-y-4">
+            @foreach ($available_gateways as $gateway)
+                <li class="border rounded-lg p-4 {{ $gateway->chosen ? 'border-blue-600' : 'border-gray-300' }}">
+                    <label class="flex items-center space-x-2">
+                        <input type="radio" name="payment_method" value="{{ $gateway->id }}" class="accent-blue-600" {{ $gateway->chosen ? 'checked' : '' }}>
+                        <span class="font-medium text-sm text-gray-800">{{ $gateway->get_title() }}</span>
+                    </label>
 
-                @if ($gateway->has_fields() || $gateway->get_description())
-                    <div class="mt-4 ml-6 text-sm text-gray-600">
-                        {!! $gateway->get_description() !!}
-                        @php $gateway->payment_fields(); @endphp
-                    </div>
-                @endif
-            </li>
-        @endforeach
-    </ul>
+                    @if ($gateway->has_fields() || $gateway->get_description())
+                        <div class="mt-4 ml-6 text-sm text-gray-600">
+                            {!! $gateway->get_description() !!}
+                            @php $gateway->payment_fields(); @endphp
+                        </div>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
 
-    @php do_action('woocommerce_review_order_after_payment'); @endphp
+        @php do_action('woocommerce_review_order_after_payment'); @endphp
+    @endif
 </div>
