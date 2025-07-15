4da95bfe75bedd81a1d23998b6f30061c1ea381a
Cesar Luis RR <romcel87@gmail.com>
2025-07-15 12:00:32 -0500

    diseño p

--- resources/views/woocommerce/checkout/payment.blade.php
+++ resources/views/woocommerce/checkout/payment.blade.php
@@ -1,3 +1,29 @@
-<div class="woocommerce-checkout-payment">
-    
+<div class="woocommerce-checkout-payment w-full">
+    @if (WC()->cart->needs_payment())
+        @php
+            do_action('woocommerce_review_order_before_payment');
+            $available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+        @endphp
+    <li class="border rounded-lg p-4 w-full {{ $gateway->chosen ? 'border-blue-600' : 'border-gray-300' }}">
+
+        <ul class="space-y-4">
+            @foreach ($available_gateways as $gateway)
+                
+                    <label class="flex items-center space-x-2">
+                        <input type="radio" name="payment_method" value="{{ $gateway->id }}" class="accent-blue-600" {{ $gateway->chosen ? 'checked' : '' }}>
+                        <span class="font-medium text-sm text-gray-800">{{ $gateway->get_title() }}</span>
+                    </label>
+
+                    @if ($gateway->has_fields() || $gateway->get_description())
+                        <div class="text-sm text-gray-600 w-full">
+                            {!! $gateway->get_description() !!}
+                            {!! $gateway->payment_fields() !!}
+                        </div>
+                    @endif
+                
+            @endforeach
+        </ul>
+    </li>
+        @php do_action('woocommerce_review_order_after_payment'); @endphp
+    @endif
 </div>
