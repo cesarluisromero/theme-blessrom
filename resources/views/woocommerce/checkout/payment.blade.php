3941f700947bc54d1af6642d525e7ae8d3ae5722
Cesar Luis RR <romcel87@gmail.com>
2025-07-15 11:54:49 -0500

    diseño payment

--- resources/views/woocommerce/checkout/payment.blade.php
+++ resources/views/woocommerce/checkout/payment.blade.php
@@ -1,28 +1,3 @@
 <div class="woocommerce-checkout-payment">
-    @if (WC()->cart->needs_payment())
-        @php
-            do_action('woocommerce_review_order_before_payment');
-            $available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
-        @endphp
-
-        <ul class="space-y-4">
-            @foreach ($available_gateways as $gateway)
-                
-                    <label class="flex items-center space-x-2">
-                        <input type="radio" name="payment_method" value="{{ $gateway->id }}" class="accent-blue-600" {{ $gateway->chosen ? 'checked' : '' }}>
-                        <span class="font-medium text-sm text-gray-800">{{ $gateway->get_title() }}</span>
-                    </label>
-
-                    @if ($gateway->has_fields() || $gateway->get_description())
-                        <div class="text-sm text-gray-600 w-full">
-                            {!! $gateway->get_description() !!}
-                            {!! $gateway->payment_fields() !!}
-                        </div>
-                    @endif
-                
-            @endforeach
-        </ul>
-
-        @php do_action('woocommerce_review_order_after_payment'); @endphp
-    @endif
+    
 </div>
