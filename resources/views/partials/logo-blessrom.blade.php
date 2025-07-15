a7bf3c74e9e8953f953141461c475b8170cf2d2f
Cesar Luis RR <romcel87@gmail.com>
2025-07-15 18:02:14 -0500

    logo

--- resources/views/partials/logo-blessrom.blade.php
+++ resources/views/partials/logo-blessrom.blade.php
@@ -1,4 +1,10 @@
-{{-- Logo --}}
-<div class="flex items-center gap-4">
+{{-- Logo Responsive --}}
+{{-- Versión móvil --}}
+<div class="flex items-center gap-4 lg:hidden">
+    <a href="{{ home_url('/') }}" class="text-white text-xl font-bold whitespace-nowrap">Blessrom</a>
+</div>
+
+{{-- Versión escritorio --}}
+<div class="hidden lg:flex items-center gap-4">
     <a href="{{ home_url('/') }}" class="text-white text-2xl font-bold whitespace-nowrap">Blessrom</a>
 </div>
