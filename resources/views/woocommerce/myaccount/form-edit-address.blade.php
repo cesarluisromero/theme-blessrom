0af7433cfe888a69132ce1b2fcef6a6b3fc0f2c0
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 13:51:43 -0500

    renderización manual

--- resources/views/woocommerce/myaccount/form-edit-address.blade.php
+++ resources/views/woocommerce/myaccount/form-edit-address.blade.php
@@ -1,25 +1,35 @@
 @php defined('ABSPATH') || exit; @endphp
 
-<form method="POST" class="space-y-6">
+<form method="post" class="space-y-6">
   @php do_action('woocommerce_before_edit_address_form_' . $load_address); @endphp
 
   {{-- Iterar campos del formulario --}}
-  @if (is_array($address))
+  @if (!empty($address) && is_array($address))
     @foreach ($address as $key => $field)
       <div class="mb-4">
-        @if (function_exists('woocommerce_form_field'))
-          {!! woocommerce_form_field(
-            $key,
-            $field,
-            wc_get_post_data_by_key($key, $field['value'])
-          ) !!}
-        @else
-          <p class="text-red-500 text-sm">No se puede mostrar el campo: función no disponible.</p>
+        <label for="{{ $key }}" class="block font-medium text-gray-700 mb-1">
+          {{ $field['label'] ?? ucfirst($key) }}
+          @if (!empty($field['required']))
+            <span class="text-red-500">*</span>
+          @endif
+        </label>
+
+        <input
+          type="{{ $field['type'] ?? 'text' }}"
+          name="{{ $key }}"
+          id="{{ $key }}"
+          class="w-full border-gray-300 rounded-lg shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50 p-2"
+          value="{{ old($key, $field['value'] ?? '') }}"
+          @if (!empty($field['required'])) required @endif
+        >
+
+        @if (!empty($field['description']))
+          <p class="text-sm text-gray-500 mt-1">{{ $field['description'] }}</p>
         @endif
       </div>
     @endforeach
   @else
-    <p class="text-red-500 text-sm">No se pudieron cargar los campos de dirección.</p>
+    <p class="text-red-500">No se pudieron cargar los campos del formulario.</p>
   @endif
 
   @php do_action('woocommerce_after_edit_address_form_' . $load_address); @endphp
