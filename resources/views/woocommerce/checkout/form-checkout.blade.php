3e7131e4c652a3bbe1b0c8ad28c06e4925da8f56
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 10:52:09 -0500

    corrigiendo errores

--- resources/views/woocommerce/checkout/form-checkout.blade.php
+++ resources/views/woocommerce/checkout/form-checkout.blade.php
@@ -36,7 +36,9 @@
                         
                         {{-- Resumen y Totales + botón --}}
                         <div id="order_review" class="w-full">
-                             @php do_action('woocommerce_checkout_order_review'); @endphp
+                            @include('woocommerce.checkout.partials.review-order')
+
+                            @include('woocommerce.checkout.payment')
 
                             {{-- Botón realizar pedido --}}
                             <div class="pt-4">
