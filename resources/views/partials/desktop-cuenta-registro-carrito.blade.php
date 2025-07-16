138d7535e4121e9e5f52d647c502726b6ed59b54
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 11:59:12 -0500

    acceso en mobil y desktop a la pagina mi cuenta

--- resources/views/partials/desktop-cuenta-registro-carrito.blade.php
+++ resources/views/partials/desktop-cuenta-registro-carrito.blade.php
@@ -2,7 +2,7 @@
       {{-- Íconos --}}
       <div class="flex items-center space-x-3">
         <a href="#" class="hover:underline">Registrate</a>
-        <a href="#" class="hover:underline">Mi cuenta</a>
+        <a href="https://blessrom.com/mi-cuenta/" class="hover:underline">Mi cuenta</a>
         <a href="https://blessrom.com/carrito/" class="relative hover:underline widget_shopping_cart_content bg-white text-white p-2 rounded-full">
           🛒
           <span id="cart-count" class="absolute -top-2 -right-2 bg-[#FFB816] text-white text-xs font-bold rounded-full px-1.5">
--- resources/views/partials/mobile-cuenta-registro-carrito.blade.php
+++ resources/views/partials/mobile-cuenta-registro-carrito.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center gap-3">
-      <a href="#" class="hover:underline text-sm">Mi cuenta</a>
+      <a href="https://blessrom.com/mi-cuenta/" class="hover:underline text-sm">Mi cuenta</a>
       <a href="https://blessrom.com/carrito/" class="relative">
         🛒
         <span id="cart-count" class="absolute -top-2 -right-2 bg-[#FFB816] text-white text-xs font-bold rounded-full px-1.5">
