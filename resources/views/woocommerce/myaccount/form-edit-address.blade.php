2587b80f587db6df03e743ddf3a02b2b1046dba7
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 13:01:19 -0500

    form-edit

--- resources/views/woocommerce/myaccount/form-edit-address.blade.php
+++ resources/views/woocommerce/myaccount/form-edit-address.blade.php
@@ -2,16 +2,13 @@
   defined('ABSPATH') || exit;
 @endphp
 
-<form method="post" class="woocommerce-EditAddress bg-white p-6 rounded-xl shadow space-y-4">
-  @method('POST')
-
-  {{-- Acción antes del formulario --}}
+<form method="POST" class="space-y-6">
   @php do_action('woocommerce_before_edit_address_form_' . $load_address); @endphp
 
   {{-- Iterar campos --}}
   @foreach ($address as $key => $field)
     <div class="mb-4">
-      @php
+      <?php
         if (function_exists('woocommerce_form_field')) {
           echo woocommerce_form_field(
             $key,
@@ -19,20 +16,15 @@
             wc_get_post_data_by_key($key, $field['value'])
           );
         }
-      @endphp
+      ?>
     </div>
   @endforeach
 
-  {{-- Acción después del formulario --}}
   @php do_action('woocommerce_after_edit_address_form_' . $load_address); @endphp
 
-  {{-- Botón submit --}}
-  <div>
-    <button type="submit"
-      class="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-xl transition-all duration-200">
-      Guardar dirección
-    </button>
-  </div>
-
-  @php wp_nonce_field('woocommerce-edit_address'); @endphp
+  <button type="submit"
+    class="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-xl transition-all duration-200">
+    Guardar dirección
+  </button>
 </form>
+
