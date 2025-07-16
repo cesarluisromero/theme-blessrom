fb7dbb966ab367ad3fbeac20665eeebdda17ea1c
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 10:50:04 -0500

    corrección de errores

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+    @php(do_action('get_header'))
+    @php(wp_head())
     <style>
       html, body {
         margin: 0 !important;
