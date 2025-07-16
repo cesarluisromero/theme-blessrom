bdcd2dc699354622cca486f8301b20c48b0ecb25
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 13:04:33 -0500

    form

--- resources/views/woocommerce/myaccount/form-edit-address.blade.php
+++ resources/views/woocommerce/myaccount/form-edit-address.blade.php
@@ -5,27 +5,26 @@
 <form method="POST" class="space-y-6">
   @php do_action('woocommerce_before_edit_address_form_' . $load_address); @endphp
 
-  {{-- Iterar campos --}}
+  {{-- Iterar campos del formulario --}}
   @foreach ($address as $key => $field)
     <div class="mb-4">
-      @php
-        if (function_exists('woocommerce_form_field')) {
-          echo woocommerce_form_field(
-            $key,
-            $field,
-            wc_get_post_data_by_key($key, $field['value'])
-          );
-        }
-      @endphp
+      {!! function_exists('woocommerce_form_field')
+          ? woocommerce_form_field(
+              $key,
+              $field,
+              wc_get_post_data_by_key($key, $field['value'])
+            )
+          : '' !!}
     </div>
   @endforeach
 
   @php do_action('woocommerce_after_edit_address_form_' . $load_address); @endphp
 
   <button type="submit"
-    class="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-xl transition-all duration-200">
+    class="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-xl text-sm transition-all duration-200">
     Guardar dirección
   </button>
 </form>
+
 
 
