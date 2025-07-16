4c80b2cd4a9fc304f1b3a592d4db4cee69fdf8c5
Cesar Luis RR <romcel87@gmail.com>
2025-07-16 11:56:07 -0500

    acceso al carrito en mobil desde el icono carrito

--- resources/views/partials/mobile-cuenta-registro-carrito.blade.php
+++ resources/views/partials/mobile-cuenta-registro-carrito.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-3">
       <a href="#" class="hover:underline text-sm">Mi cuenta</a>
-      <a href="#" class="relative">
+      <a href="https://blessrom.com/carrito/" class="relative">
         🛒
         <span id="cart-count" class="absolute -top-2 -right-2 bg-[#FFB816] text-white text-xs font-bold rounded-full px-1.5">
           {{ WC()->cart->get_cart_contents_count() }}
