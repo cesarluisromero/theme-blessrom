e4b414eb8d49687f94489ffaf1f0d6e22b7493f7
Cesar Luis RR <romcel87@gmail.com>
2025-07-17 11:16:15 -0500

    reset

--- app/setup.php
+++ app/setup.php
@@ -87,6 +87,46 @@ add_action('after_setup_theme', function () {
 
 }, 20);
 
+// Redirige si recibe ?key=...&login=...
+add_action('template_redirect', function () {
+    global $wp;
+    if (
+        isset($wp->query_vars['lost-password']) &&
+        isset($_GET['key']) &&
+        isset($_GET['login'])
+    ) {
+        $key = sanitize_text_field($_GET['key']);
+        $login = sanitize_text_field($_GET['login']);
+        $id = isset($_GET['id']) ? intval($_GET['id']) : null; // opcional, si lo usas
+        $url = wc_get_account_endpoint_url('reset-password') . "?key={$key}&login={$login}";
+        if ($id) {
+            $url .= "&id={$id}";
+        }
+        wp_redirect($url);
+        exit;
+    }
+});
+
+// Registro de endpoint reset-password
+add_action('init', function () {
+    add_rewrite_endpoint('reset-password', EP_ROOT | EP_PAGES);
+});
+
+// WooCommerce login redirect
+add_filter('woocommerce_login_redirect', function($redirect, $user) {
+    // Si hay una redirección almacenada en la URL (por ejemplo, checkout), redirige allí
+    if (isset($_GET['redirect_to']) && !empty($_GET['redirect_to'])) {
+        return esc_url($_GET['redirect_to']);
+    }
+    // Por defecto, redirige al checkout si el carrito no está vacío
+    if (WC()->cart && !WC()->cart->is_empty()) {
+        return wc_get_checkout_url();
+    }
+    // Si no hay nada en el carrito, redirige a la cuenta
+    return wc_get_page_permalink('myaccount');
+}, 10, 2);
+
+//carga la página del producto
 add_filter('template_include', function ($template) {
         if (is_singular('product')) {
             $blade_template = locate_template('resources/views/woocommerce/single-product.blade.php');
@@ -96,52 +136,40 @@ add_filter('template_include', function ($template) {
             }
         }
         return $template;
-    }, 99);
+}, 99);
 
-    add_filter('template_include', function ($template) {
-        if (is_checkout() && !is_order_received_page()) {
-            $blade_template = locate_template('resources/views/woocommerce/checkout/form-checkout.blade.php');
-            if ($blade_template) {
-                echo \Roots\view('woocommerce.checkout.form-checkout')->render();
-                exit; // Detiene el flujo de carga de otras plantillas
-            }
+//redirige al checkout
+add_filter('template_include', function ($template) {
+    if (is_checkout() && !is_order_received_page()) {
+        $blade_template = locate_template('resources/views/woocommerce/checkout/form-checkout.blade.php');
+        if ($blade_template) {
+            echo \Roots\view('woocommerce.checkout.form-checkout')->render();
+            exit; // Detiene el flujo de carga de otras plantillas
         }
-        return $template;
-    }, 99);
+    }
+    return $template;
+}, 99);
 
-    add_filter('woocommerce_login_redirect', function($redirect, $user) {
-        // Si hay una redirección almacenada en la URL (por ejemplo, checkout), redirige allí
-        if (isset($_GET['redirect_to']) && !empty($_GET['redirect_to'])) {
-            return esc_url($_GET['redirect_to']);
+
+//redirige a la página de ordenes
+add_filter('template_include', function ($template) {
+    if (is_order_received_page()) {
+        $order_id = absint(get_query_var('order-received'));
+        $order = wc_get_order($order_id);
+
+        if ($order) {
+            echo \Roots\view('woocommerce.thankyou', [
+                'order' => $order,
+            ])->render();
+            exit;
         }
+    }
 
-        // Por defecto, redirige al checkout si el carrito no está vacío
-        if (WC()->cart && !WC()->cart->is_empty()) {
-            return wc_get_checkout_url();
-        }
+    return $template;
+}, 99);
 
-        // Si no hay nada en el carrito, redirige a la cuenta
-        return wc_get_page_permalink('myaccount');
-    }, 10, 2);
-
-    add_filter('template_include', function ($template) {
-        if (is_order_received_page()) {
-            $order_id = absint(get_query_var('order-received'));
-            $order = wc_get_order($order_id);
-
-            if ($order) {
-                echo \Roots\view('woocommerce.thankyou', [
-                    'order' => $order,
-                ])->render();
-                exit;
-            }
-        }
-
-        return $template;
-    }, 99);
-
-
-    add_filter('template_include', function ($template) {
+//página para loguearse
+add_filter('template_include', function ($template) {
     // Mostrar solo en la página "Mi cuenta" (login/register)
     if (is_account_page() && !is_user_logged_in()) {
         echo \Roots\view('woocommerce.myaccount.form-login')->render();
@@ -149,9 +177,10 @@ add_filter('template_include', function ($template) {
     }
 
     return $template;
-    }, 99);
+}, 99);
 
-    add_filter('template_include', function ($template) {
+//pagina logueada
+add_filter('template_include', function ($template) {
     if (is_account_page() && !is_wc_endpoint_url()) {
         $blade_template = locate_template('resources/views/woocommerce/myaccount/dashboard.blade.php');
         if ($blade_template) {
@@ -160,17 +189,19 @@ add_filter('template_include', function ($template) {
         }
     }
     return $template;
-    }, 99);
+}, 99);
 
-    add_filter('template_include', function ($template) {
+//página para editar direccion
+add_filter('template_include', function ($template) {
     if (is_account_page() && is_wc_endpoint_url('edit-address')) {
         echo \Roots\view('woocommerce.myaccount.edit-address')->render();
         exit;
     }
     return $template;
-    }, 100);
+}, 100);
 
-    add_filter('template_include', function ($template) {
+//página para editar direccion
+add_filter('template_include', function ($template) {
     if (is_wc_endpoint_url('edit-address')) {
         $blade_template = locate_template('resources/views/woocommerce/myaccount/form-edit-address.blade.php');
         if ($blade_template) {
@@ -182,114 +213,94 @@ add_filter('template_include', function ($template) {
         }
     }
     return $template;
-    }, 99);
+}, 99);
 
-     add_filter('template_include', function ($template) {
-        if (is_wc_endpoint_url('edit-account')) {
-            $blade_template = locate_template('resources/views/woocommerce/myaccount/form-edit-account.blade.php');
+//pagina para editar cuenta
+add_filter('template_include', function ($template) {
+    if (is_wc_endpoint_url('edit-account')) {
+        $blade_template = locate_template('resources/views/woocommerce/myaccount/form-edit-account.blade.php');
 
-            if ($blade_template) {
-                echo \Roots\view('woocommerce.myaccount.form-edit-account', [
-                    'user' => wp_get_current_user(),
-                    'nonce_value' => wp_create_nonce('save_account_details'),
-                ])->render();
-                exit;
-            }
-        }
-
-        return $template;
-    }, 99);
-
-    add_filter('template_include', function ($template) {
-        if (is_wc_endpoint_url('orders')) {
-            $blade_template = locate_template('resources/views/woocommerce/myaccount/orders.blade.php');
-
-            if ($blade_template) {
-                $current_page = max(1, get_query_var('paged'));
-                $customer_orders = wc_get_orders([
-                    'customer_id' => get_current_user_id(),
-                    'paginate' => true,
-                    'paged' => $current_page,
-                    'limit' => 10,
-                ]);
-
-                echo \Roots\view('woocommerce.myaccount.orders', [
-                    'orders' => $customer_orders,
-                    'current_page' => $current_page,
-                ])->render();
-                exit;
-            }
-        }
-
-        return $template;
-    }, 99);
-
-    add_filter('template_include', function ($template) {
-        if (is_wc_endpoint_url('view-order')) {
-            $blade_template = locate_template('resources/views/woocommerce/myaccount/view-order.blade.php');
-            if ($blade_template) {
-                global $wp;
-                $order_id = absint($wp->query_vars['view-order']);
-                $order = wc_get_order($order_id);
-
-                if ($order) {
-                    echo \Roots\view('woocommerce.myaccount.view-order', [
-                        'order' => $order,
-                    ])->render();
-                    exit;
-                }
-            }
-        }
-        return $template;
-    }, 99);
-
-    add_action('template_redirect', function () {
-        global $wp;
-
-        if (is_account_page() && isset($wp->query_vars['lost-password'])) {
-            echo \Roots\view('woocommerce.myaccount.form-lost-password')->render();
+        if ($blade_template) {
+            echo \Roots\view('woocommerce.myaccount.form-edit-account', [
+                'user' => wp_get_current_user(),
+                'nonce_value' => wp_create_nonce('save_account_details'),
+            ])->render();
             exit;
         }
-    });
-    
+    }
 
-    add_filter('template_include', function ($template) {
-        global $wp;
+    return $template;
+}, 99);
 
-        if (is_wc_endpoint_url('reset-password')) {
-            $blade_template = locate_template('resources/views/woocommerce/myaccount/form-reset-password.blade.php');
-            if ($blade_template) {
-                echo \Roots\view('woocommerce.myaccount.form-reset-password')->render();
+//página de ordenes
+add_filter('template_include', function ($template) {
+    if (is_wc_endpoint_url('orders')) {
+        $blade_template = locate_template('resources/views/woocommerce/myaccount/orders.blade.php');
+
+        if ($blade_template) {
+            $current_page = max(1, get_query_var('paged'));
+            $customer_orders = wc_get_orders([
+                'customer_id' => get_current_user_id(),
+                'paginate' => true,
+                'paged' => $current_page,
+                'limit' => 10,
+            ]);
+
+            echo \Roots\view('woocommerce.myaccount.orders', [
+                'orders' => $customer_orders,
+                'current_page' => $current_page,
+            ])->render();
+            exit;
+        }
+    }
+
+    return $template;
+}, 99);
+
+//página para ver toddas las ordenes
+add_filter('template_include', function ($template) {
+    if (is_wc_endpoint_url('view-order')) {
+        $blade_template = locate_template('resources/views/woocommerce/myaccount/view-order.blade.php');
+        if ($blade_template) {
+            global $wp;
+            $order_id = absint($wp->query_vars['view-order']);
+            $order = wc_get_order($order_id);
+
+            if ($order) {
+                echo \Roots\view('woocommerce.myaccount.view-order', [
+                    'order' => $order,
+                ])->render();
                 exit;
             }
         }
+    }
+    return $template;
+}, 99);
 
-        return $template;
-    }, 99);
-
-
+//página cuando se olvida contraseña
 add_action('template_redirect', function () {
-        global $wp;
+    global $wp;
 
-        if (
-            isset($wp->query_vars['lost-password']) &&
-            isset($_GET['key']) &&
-            isset($_GET['login'])
-        ) {
-            $key = sanitize_text_field($_GET['key']);
-            $login = sanitize_text_field($_GET['login']);
-            $id = isset($_GET['id']) ? intval($_GET['id']) : null; // opcional, si lo usas
+    if (is_account_page() && isset($wp->query_vars['lost-password'])) {
+        echo \Roots\view('woocommerce.myaccount.form-lost-password')->render();
+        exit;
+    }
+});
+    
+//cambio de contraseña
+add_filter('template_include', function ($template) {
+    global $wp;
 
-            $url = wc_get_account_endpoint_url('reset-password') . "?key={$key}&login={$login}";
-
-            if ($id) {
-                $url .= "&id={$id}";
-            }
-
-            wp_redirect($url);
+    if (is_wc_endpoint_url('reset-password')) {
+        $blade_template = locate_template('resources/views/woocommerce/myaccount/form-reset-password.blade.php');
+        if ($blade_template) {
+            echo \Roots\view('woocommerce.myaccount.form-reset-password')->render();
             exit;
         }
-});
+    }
+
+    return $template;
+}, 99);
 
 /**
  * Register the theme sidebars.
