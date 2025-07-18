a8c108792ee1dfdff1e459d64cc61e8757b1ab96
Cesar Luis RR <romcel87@gmail.com>
2025-07-18 12:32:05 -0500

    reset

--- resources/views/woocommerce/myaccount/form-reset-password.blade.php
+++ resources/views/woocommerce/myaccount/form-reset-password.blade.php
@@ -1,5 +1,19 @@
-@extends('layouts.app')
 <?php
+/**
+ * Lost password reset form.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/form-reset-password.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.2.0
+ */
 
 defined( 'ABSPATH' ) || exit;
 
