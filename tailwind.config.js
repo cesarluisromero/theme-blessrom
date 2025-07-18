de6ed9676fe594740728a936b93ddb890c60993b
Cesar Luis RR <romcel87@gmail.com>
2025-07-18 12:01:49 -0500

    Se agrego archivo tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/scripts/**/*.js',
+    './app/**/*.php',
+    './woocommerce/**/*.php',
+    './*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
