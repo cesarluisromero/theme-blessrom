cb25cf946f0162121206ff1ca92c7640bbad8c42
Cesar Luis RR <romcel87@gmail.com>
2025-07-20 06:31:27 -0500

    orders

--- app/setup.php
+++ app/setup.php
@@ -152,30 +152,6 @@ add_filter('template_include', function ($template) {
     return $template;
 }, 99);
 
-//página de ordenes
-add_filter('template_include', function ($template) {
-    if (is_wc_endpoint_url('orders')) {
-        $blade_template = locate_template('resources/views/woocommerce/myaccount/orders.blade.php');
-
-        if ($blade_template) {
-            $current_page = max(1, get_query_var('paged'));
-            $customer_orders = wc_get_orders([
-                'customer_id' => get_current_user_id(),
-                'paginate' => true,
-                'paged' => $current_page,
-                'limit' => 10,
-            ]);
-
-            echo \Roots\view('woocommerce.myaccount.orders', [
-                'orders' => $customer_orders,
-                'current_page' => $current_page,
-            ])->render();
-            exit;
-        }
-    }
-
-    return $template;
-}, 99);
 
 /**
  * Register the theme sidebars.
